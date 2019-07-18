9db93ef6989e0e36382aada0d2470b1199ebb540
Richard Allaway <richard.allaway@gmail.com>
2019-07-18 15:25:33 -0400

    render search component, setup initial parameters with behavior subject

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { debug } from '../helpers/rxjs-helpers';
+import { SearchParams } from '../interfaces/search-params';
+import { Order } from '../services/order.enum';
+import { Sort } from '../services/sort.enum';
+import { GithubService } from '../services/github.service';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  constructor(
+    private githubService: GithubService
+  ) {}
 
-  constructor() {}
+  searchParamsSubject: BehaviorSubject<SearchParams> = new BehaviorSubject({
+    term: '',
+    sort: Sort.repos,
+    order: undefined,
+    page: 1,
+    perPage: 20,
+    showMatch: false
+  })
 
+  ngOnInit() {
+    this.searchParamsSubject.pipe(
+      debug('searchParamsSubject')
+    ).subscribe();
+  }
+
+  doSearch(value:boolean) {
+    console.log('do search');
+    this.githubService.triggerSubject.next(value);
+  }
 }
