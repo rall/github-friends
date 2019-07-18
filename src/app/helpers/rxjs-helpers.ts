f16cb8898d49d921e209948660b0e2b76964cc47
Richard Allaway <richard.allaway@gmail.com>
2019-07-17 20:52:31 -0400

    some rxjs helper operators

---
+++ src/app/helpers/rxjs-helpers.ts
@@ -0,0 +1,46 @@
+import { Observable, fromEventPattern } from 'rxjs';
+import { tap, switchMap, filter, map, mapTo, withLatestFrom } from 'rxjs/operators';
+
+export function debug<T>(message:any) {
+    return (observable$: Observable<T>): Observable<T> => {
+        return observable$.pipe(
+            tap<T>(val => console.info(message, val), console.error, () => console.log(message, 'COMPLETED'))
+        );
+    }
+}
+
+export function filterTrue<T>() {
+    return (observable$: Observable<T>): Observable<T> => {
+        return observable$.pipe(
+            filter<T>(Boolean),
+        );
+    }
+}
+
+export function filterFalse<T>() {
+    return (observable$: Observable<T>): Observable<T> => {
+        return observable$.pipe(
+            filter<T>(val => !val),
+        );
+    }
+}
+
+interface Collection {
+    length: number;
+}
+
+export function filterEmpty<T extends Collection>() {
+    return (observable$: Observable<T>): Observable<T> => {
+        return observable$.pipe(
+            filter<T>(collection => collection.length === 0)
+        );
+    }
+}
+
+export function filterPresent<T extends Collection>() {
+    return (observable$: Observable<T>): Observable<T> => {
+        return observable$.pipe(
+            filter<T>(collection => collection.length > 0)
+        );
+    }
+}
