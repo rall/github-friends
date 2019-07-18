bab4b4b5cb4d5203860e6e1516460488588ce81b
Richard Allaway <richard.allaway@gmail.com>
2019-07-18 19:01:32 -0400

    set search params in service not page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,9 +1,6 @@
-import { Component, OnInit } from '@angular/core';
-import { BehaviorSubject } from 'rxjs';
-import { debug } from '../helpers/rxjs-helpers';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import { Subject } from 'rxjs';
 import { SearchParams } from '../interfaces/search-params';
-import { Order } from '../services/order.enum';
-import { Sort } from '../services/sort.enum';
 import { GithubService } from '../services/github.service';
 
 @Component({
@@ -11,28 +8,17 @@ import { GithubService } from '../services/github.service';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage implements OnInit {
+export class HomePage {
   constructor(
     private githubService: GithubService
-  ) {}
+  ) {
 
-  searchParamsSubject: BehaviorSubject<SearchParams> = new BehaviorSubject({
-    term: '',
-    sort: Sort.repos,
-    order: undefined,
-    page: 1,
-    perPage: 20,
-    showMatch: false
-  })
-
-  ngOnInit() {
-    this.searchParamsSubject.pipe(
-      debug('searchParamsSubject')
-    ).subscribe();
+    
   }
 
+  searchParamsSubject: Subject<SearchParams> = new Subject();
+
   doSearch(value:boolean) {
-    console.log('do search');
     this.githubService.triggerSubject.next(value);
   }
 }
